5c116d30c3457bde111f39a8e46e4625aa63f856
Robin Brase <rho2bin@gmail.com>
2019-11-24 02:18:54 +0100

    Added NN that uses the human collected data

---
+++ nn_human.py
@@ -0,0 +1,24 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, GaussianNoise, Dropout
+import data
+
+N = len(data.human_data[0])
+print(N)
+
+model = Sequential()
+model.add(Dense(N))
+
+model.add(Dense(10))
+
+model.add(Dense(5, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='mean_absolute_error',
+              metrics=['accuracy'])
+
+n_data = np.array(data.human_data)
+n_label = np.array(data.out_norm)
+
+model.fit(n_data, n_label, epochs=100, batch_size=1, verbose=1)
+model.save('human.h5')
